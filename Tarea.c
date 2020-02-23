2e61e3a7bd5415a34ac9729abc33aed82dde9362
BuildTools Willard <willard@buildtools.com>
2020-02-23 02:42:23 -0600

    Tarea 1 lista

--- Tarea.c
+++ Tarea.c
@@ -1,360 +1,243 @@
-/* 
- * File:   main.c
- * Author: Brayan
- *
- * Created on 11 de marzo de 2015, 02:07 PM
- */
+/*
+* File: main.c
+* Author: Willard Zamora
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <math.h>
+#include <string.h>
 
-struct Puente {
-    int canCarrosPuente;
-    int direccionPuente;
-    int tamPuente;
-    pthread_mutex_t *arreglo;
+struct Bridge {
+    int length;
+    int direction;
+    int occupied;
+    char* state;
+    pthread_mutex_t* spot;
 };
-typedef enum {true, false} bool;
 
+struct Vehicle{
+    int direction;
+};
 
-int semaforo = 0;
-int tiempoSemaforoEste;
-int tiempoSemaforoOeste;
+double expMeanW;
+double expMeanE;
+int vehiclesW;
+int vehiclesE;
+int vehiclesWO;
+int vehiclesEO;
+int waitingW;
+int waitingE;
+int speed;
 
+pthread_mutex_t bridgeOccupied;
+pthread_mutex_t yieldDirection;
+pthread_mutex_t sysOut;
+pthread_cond_t directionChanged;
 
-int mediaExponencial;
-int velocidadMedia;
+struct Bridge bridge;
 
-int totalOeste;
-int totalEste;
-
-pthread_mutex_t canAmbOE;
-pthread_mutex_t canAmbEO;
-
-pthread_mutex_t mutexPuente;
-pthread_mutex_t mutexEO;
-pthread_mutex_t mutexOE;
-pthread_cond_t condEO;
-pthread_cond_t condOE;
-
-pthread_mutex_t mutex;
-pthread_cond_t cond;
-int esperaoeste=0;
-int esperaeste=0;
-int pasadoeste=0;
-int pasadooeste=0;
-int dirTrafico = 0;
-
-pthread_mutex_t imprimir;
-
-struct Puente puente;
-char* puenteGrafic;
-
-int tiempoExponencial();
-int aleatorio();
-void *generarCarrosOE(void*X);
-void *generarCarrosEO(void*X);
-
-void *moverOesteEste(void*p);
-void *moverEsteOeste(void*p);
-void cambiarDireccionAumentarPuente(int valor);
-void cambiarDireccionPuente(int valor);
-void aumentarCarrosPuente();
-void disminuirCarrosPuente();
-
-int bolquearPosPuente(int pos,char caracter);
-void desboloquearPosPuente(int pos);
-void imprimirEstado();
-
-void *cambiarSemaforo(void*X);
-void adminSemaforoOE();
-void adminSemaforoEO();
-
-void adminCedaOE();
-void adminCedaEO();
-void cambiaraoeste();
-void cambiaraeste();
-void *ceda(void*X);
-
-void adminTraficoOE();
-void adminTraficoEO();
-void *trafico(void*X);
+void* spawnW();
+void* spawnE();
+void* crossBridge(void* vehicle);
+void printState();
+void parseAndValidateParams();
 
 int main(int argc, char** argv) {
-
-    FILE *f = fopen("Datos.txt", "r");
-    if(f==NULL){
-        printf("%s\n","No se pudo leer el archivo");
-    }else{
-        fscanf (f, "%d", &puente.tamPuente);
-        fscanf (f, "%d", &mediaExponencial);
-        fscanf (f, "%d", &velocidadMedia);
-    }
-
-    fclose(f);
+    parseAndValidateParams(argc, argv);
     
-    puente.canCarrosPuente = 0;
-    puente.direccionPuente = 0;
-    puente.arreglo = malloc(puente.tamPuente * sizeof (pthread_mutex_t));
-    puenteGrafic = (char *) malloc(puente.tamPuente);
+    bridge.occupied = 0;
+    bridge.direction = 1;
+    bridge.spot = malloc(bridge.length * sizeof (pthread_mutex_t));
+    bridge.state = (char *) malloc(bridge.length);
 
-    long i;
-    for (i = 0; i < puente.tamPuente; i++) {
-        pthread_mutex_init(&puente.arreglo[i], NULL);
-        puenteGrafic[i] = '_';
+    for (int i = 0; i < bridge.length; i++) {
+        pthread_mutex_init(&bridge.spot[i], NULL);
+        bridge.state[i] = '_';
     }
-
-    pthread_mutex_init(&imprimir, NULL);
-    pthread_mutex_init(&canAmbOE,NULL);
-    pthread_mutex_init(&canAmbEO,NULL);
-    pthread_mutex_init(&mutexEO, NULL);
-    pthread_mutex_init(&mutexOE, NULL);
-    pthread_mutex_init(&mutexPuente, NULL);
-    pthread_cond_init(&condEO, NULL);
-    pthread_cond_init(&condOE, NULL);
-
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond, NULL);
-
-    pthread_t hiloAdmin;
-    pthread_t generarOE;
-    pthread_t generarEO;
-
-    void *status;
-    pthread_attr_t atributo;
-    pthread_attr_init(&atributo);
-    pthread_attr_setdetachstate(&atributo, PTHREAD_CREATE_JOINABLE);
-
-    puente.direccionPuente = 0;
-    pthread_create(&hiloAdmin, &atributo, ceda, NULL);
-    pthread_create(&generarEO, &atributo, generarCarrosEO, NULL);
-    pthread_create(&generarOE, &atributo, generarCarrosOE, NULL);
-    pthread_attr_destroy(&atributo);
-
-    pthread_exit(NULL);
-}
-
-int tiempoExponencial() {
+    
     srand(time(NULL));
-    double me = 0.0f;
-    int res = 0;
-    do {
-        double n = 0 + (1 - 0) * rand() / ((double) RAND_MAX);
-        double resultado = log(1 - n);
-        me = (-mediaExponencial) * resultado;
-        res = me;
-    } while (res == 0 || res > 9);
-    return res;
-}
+    
+    pthread_mutex_init(&bridgeOccupied, NULL);
+    pthread_mutex_init(&yieldDirection, NULL);
+    pthread_mutex_init(&sysOut, NULL);
+    pthread_cond_init(&directionChanged, NULL);
+    
+    pthread_t spawnerW;
+    pthread_t spawnerE;
 
-int aleatorio() {
-    srand(time(NULL));
-    return rand() % 2;
-}
+    pthread_attr_t joinable;
+    pthread_attr_init(&joinable);
+    pthread_attr_setdetachstate(&joinable, PTHREAD_CREATE_JOINABLE);
 
-void *ceda(void*X) {
-    while (1) {
-        if(esperaoeste > 0 && esperaeste == 0 && puente.direccionPuente == 0)
-            cambiaraoeste();
-        if(esperaeste > 0 && esperaoeste == 0 && puente.direccionPuente == 1)
-            cambiaraeste();
-    }
-}
+    pthread_create(&spawnerW, &joinable, spawnW, NULL);
+    pthread_create(&spawnerE, &joinable, spawnE, NULL);
+    pthread_attr_destroy(&joinable);
 
-void cambiaraoeste(){
-    while(puente.canCarrosPuente > 0)
-        pthread_cond_wait(&cond, &mutex);
-    printf("%s %i\n","no esperan al este y al oeste si",puente.canCarrosPuente);
-    puente.direccionPuente=1;
-    pasadooeste=0;
-    pasadoeste=0;
-    pthread_cond_broadcast(&condOE);
-}
-
-void cambiaraeste(){
-    while(puente.canCarrosPuente > 0)
-        pthread_cond_wait(&cond, &mutex);
-    printf("%s %i\n","iguales al oeste",puente.canCarrosPuente);
-    puente.direccionPuente=0;
-    pasadoeste=0;
-    pasadooeste=0;
-    pthread_cond_broadcast(&condEO);
-}
-
-void adminCedaOE(){
-    bool esperar = !puente.direccionPuente;
-    if (esperar) {
-        esperaoeste++;
-        while (!puente.direccionPuente )
-            pthread_cond_wait(&condOE, &mutexOE);
-        esperaoeste--;
-    }
-}
-
-void adminCedaEO(){
-    bool esperar = puente.direccionPuente;
-    if (esperar) {
-        esperaeste++;
-        while (puente.direccionPuente)
-            pthread_cond_wait(&condEO, &mutexEO);
-        esperaeste--;
-    }
-}
-
-void *generarCarrosOE(void*X) {
-    int res = tiempoExponencial();
-    int t;
-    int *esAmbulancia[1];
-    esAmbulancia[0] = (int *) malloc(sizeof (int));
-    t = 0;
-    while (1) {
-        *esAmbulancia[0] = 0;
-        if (t > 0 && t % 15 == 0) {
-            *esAmbulancia[0] = random();
-        }
-        pthread_t carroOE;
-        pthread_create(&carroOE, NULL, moverOesteEste, (void*) esAmbulancia[0]);
-        sleep(res);
-        t++;
-    }
     pthread_exit(NULL);
 }
 
-void *generarCarrosEO(void*X) {
-    int res = tiempoExponencial();
-    int t;
-    int *esAmbulancia[1];
-    esAmbulancia[0] = (int *) malloc(sizeof (int));
-    t = 0;
-    while (1) {
-        *esAmbulancia[0] = 0;
-        if (t > 0 && t % 15 == 0) {
-            *esAmbulancia[0] = random();
-        }
-        pthread_t carroEO;
-        pthread_create(&carroEO, NULL, moverEsteOeste, (void*) esAmbulancia[0]);
-        sleep(res);
-        t++;
-    }
-    pthread_exit(NULL);
-}
-
-void *moverOesteEste(void* p) {
-    int *id_ptr, taskid;
-    id_ptr = (int *) p;
-    taskid = *id_ptr;
-    if (!taskid) {
-         pthread_mutex_lock(&mutexOE);
-        adminCedaOE();
-        cambiarDireccionAumentarPuente(1);
-        int posActual = -1;
-        posActual = bolquearPosPuente(posActual + 1,'>');
-        sleep(velocidadMedia);
-        while (posActual < puente.tamPuente - 1) {
-            posActual = bolquearPosPuente(posActual + 1,'>');
-            desboloquearPosPuente(posActual-1);
-            if(posActual == 1){
-                pthread_mutex_unlock(&mutexOE);
+void parseAndValidateParams(int argc, char** argv){
+    char *eptr;
+    int withErrors = 0;
+    
+    expMeanE = expMeanW = 0.25;
+    speed = 1;
+    bridge.length = 5;
+    
+    if(argc == 2 && strcmp(argv[1], "-h") == 0){
+        printf("\n%s\n","Application receives up to 6 parameters, with the first two being mandatory, in the following order: \n\t Vehicles coming East : +int \n\t Vehicles coming West : +int \n\t East Spawn Mean : +double \n\t West Spawn Mean : +double \n\t Bridge Length : +int \n\t Vehicle Speed : +int\n");
+        exit(EXIT_SUCCESS);
+    }else{
+        if(argc < 3){
+            printf("\n%s\n\n","Minimum number of parameter not meet, please use -h to see usage.");
+            exit(EXIT_FAILURE);
+        }else{
+            vehiclesEO = vehiclesE = atoi(argv[1]);
+            if(vehiclesE <= 0){
+                withErrors = 1;
+                printf("\n%s\n","Vehicles coming East must be a positive integer.");
             }
-            sleep(velocidadMedia);
-        }
-        desboloquearPosPuente(posActual);
-        disminuirCarrosPuente();
-        if (puente.canCarrosPuente == 0) {
-            pthread_cond_broadcast(&condEO);
-            pthread_cond_broadcast(&cond);
-
-        }
-    } 
-    pthread_exit(NULL);
-}
-
-void *moverEsteOeste(void* p) {
-    int *id_ptr, taskid;
-    id_ptr = (int *) p;
-    taskid = *id_ptr;
-    if (!taskid) {
-        pthread_mutex_lock(&mutexEO);
-        adminCedaEO();
-        cambiarDireccionAumentarPuente(0);
-        int posActual = bolquearPosPuente(puente.tamPuente - 1, '<');
-        sleep(velocidadMedia);
-        while (posActual > 0) {
-            posActual = bolquearPosPuente(posActual - 1, '<');
-            desboloquearPosPuente(posActual+1);
-            if(posActual == puente.tamPuente-2){
-                pthread_mutex_unlock(&mutexEO);
+            vehiclesWO = vehiclesW = atoi(argv[2]);
+            if(vehiclesW <= 0){
+                withErrors = 1;
+                printf("\n%s\n","Vehicles coming West must be a positive integer.");
             }
-            sleep(velocidadMedia);
+            if(argc >= 4 ){
+                expMeanE = atof(argv[3]);
+                if(expMeanE <= 0){
+                    withErrors = 1;
+                    printf("\n%s\n","East Spawn Mean must be a positive double.");
+                }
+            }
+            if(argc >= 5){
+                expMeanW = atof(argv[4]);
+                if(expMeanW <= 0){
+                    withErrors = 1;
+                    printf("\n%s\n","West Spawn Mean must be a positive double.");
+                }
+            }
+            if(argc >= 6){
+                bridge.length = atoi(argv[5]);
+                if(bridge.length <= 0){
+                    withErrors = 1;
+                    printf("\n%s\n","Bridge Length must be a positive integer.");
+                }
+            }
+            if(argc == 7){
+                speed = atoi(argv[6]);
+                if(speed <= 0){
+                    withErrors = 1;
+                    printf("\n%s\n","Vehicle Speed must be a positive integer.");
+                }
+            }
         }
-        desboloquearPosPuente(posActual);
-        disminuirCarrosPuente();
-        if (puente.canCarrosPuente == 0) {
-            pthread_cond_broadcast(&cond);                           
-        }
-    } 
+    }
+    if(withErrors){
+        printf("\n%s\n\n","Parameters type does not match, please use -h to see usage.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+double expDist(double lambda) {
+    double u;
+    u = rand() / (RAND_MAX + 1.0);
+    return -log(1- u) / lambda;
+}
+
+void* spawnW() {
+    while (vehiclesW > 0) {
+        vehiclesW--;
+        pthread_t vehicleW;
+        struct Vehicle* vehicle = malloc(sizeof(struct Vehicle));
+        vehicle->direction = 1;
+        pthread_create(&vehicleW, NULL, crossBridge, (void*) vehicle);
+        sleep(expDist(expMeanW));
+    }
     pthread_exit(NULL);
 }
 
-
-void cambiarDireccionAumentarPuente(int valor){
-    pthread_mutex_lock(&mutexPuente);
-    // if(valor == 0)
-    //    pthread_mutex_lock(&mutexOE);
-    // else
-    //    pthread_mutex_lock(&mutexEO);
-    cambiarDireccionPuente(valor);
-    aumentarCarrosPuente();
-    // if(valor == 0)
-    // pthread_mutex_lock(&mutexOE);
-    // else
-    // pthread_mutex_lock(&mutexEO);
-    pthread_mutex_unlock(&mutexPuente);
+void* spawnE() {
+    while (vehiclesE > 0) {
+        vehiclesE--;
+        pthread_t vehicleE;
+        struct Vehicle* vehicle = malloc(sizeof(struct Vehicle));
+        vehicle->direction = -1;
+        pthread_create(&vehicleE, NULL, crossBridge, (void*) vehicle);
+        sleep(expDist(expMeanE));
+    }
+    pthread_exit(NULL);
 }
 
-void cambiarDireccionPuente(int valor){
-    puente.direccionPuente = valor;
+void* crossBridge(void* vehicle){
+    struct Vehicle* currentVehicle = (struct Vehicle*) vehicle;
+    pthread_mutex_lock(&yieldDirection);
+    if(currentVehicle->direction == 1){
+        waitingW++;
+    }else{
+        waitingE++;
+    }
+    while (currentVehicle->direction != bridge.direction){
+        pthread_cond_wait(&directionChanged, &yieldDirection);
+    }
+    pthread_mutex_unlock(&yieldDirection);
+    int position = (currentVehicle->direction == 1) ? 0 : bridge.length - 1;
+    int goingIn = 1;
+    while (position > -1 && position < bridge.length){
+        pthread_mutex_lock(&bridge.spot[position]);
+        if(goingIn){
+            pthread_mutex_lock(&bridgeOccupied);
+            bridge.occupied++;
+            pthread_mutex_unlock(&bridgeOccupied);
+            if(currentVehicle->direction == 1){
+                waitingW--;
+            }else{
+                waitingE--;
+            }
+            goingIn = 0; 
+        }
+        bridge.state[position] = (currentVehicle->direction == 1) ? '>' : '<';
+        printState();
+        sleep(speed);
+        bridge.state[position] = '_';
+        pthread_mutex_unlock(&bridge.spot[position]);
+        printState();
+        position+= currentVehicle->direction;
+    }
+    pthread_cond_broadcast(&directionChanged);
+    pthread_mutex_lock(&bridgeOccupied);
+    bridge.occupied--;
+    pthread_mutex_unlock(&bridgeOccupied);
+    pthread_mutex_lock(&yieldDirection);
+    if(!bridge.occupied && (waitingW || waitingE)){
+        bridge.direction *= -1;
+    }
+    pthread_mutex_unlock(&yieldDirection); 
+    free(currentVehicle);
+    pthread_exit(NULL);      
 }
 
-void aumentarCarrosPuente(){
-    puente.canCarrosPuente++;
-}
-
-void disminuirCarrosPuente(){
-    pthread_mutex_lock(&mutexPuente);
-    puente.canCarrosPuente--;
-    pthread_mutex_unlock(&mutexPuente);
-}
-
-int bolquearPosPuente(int pos,char caracter){
-    pthread_mutex_lock(&puente.arreglo[pos]);
-    puenteGrafic[pos] = caracter;
-    pthread_mutex_lock(&imprimir);
-    imprimirEstado();
-    pthread_mutex_unlock(&imprimir);
-    return pos;
-}
-
-void desboloquearPosPuente(int pos){
-    pthread_mutex_unlock(&puente.arreglo[pos]);
-    puenteGrafic[pos] = '_';
-    pthread_mutex_lock(&imprimir);
-    imprimirEstado();
-    pthread_mutex_unlock(&imprimir);
-}
-
-void imprimirEstado() {
+void printState() {
+    pthread_mutex_lock(&sysOut);
     system("clear");
     system("clear");
-    long i;
 
-    printf("\t%s\n","Administración CEDA");        
-    printf("\n%s %i\n","Cantidad de carros en espera al Oeste ",esperaoeste);
-    printf("\n%s %i\n","Cantidad de carros en espera al Este ",esperaeste);
-    printf("\n%s %i\n","Via actual del puente ",puente.direccionPuente);
+    printf("%s\n","Parameters");
+    printf("\n\t%s %i\n","Vehicles coming East", vehiclesEO);
+    printf("\n\t%s %i\n","Vehicles coming West", vehiclesWO);
+    printf("\n\t%s %lf\n","Vehicles East Spawn Mean", expMeanE);
+    printf("\n\t%s %lf\n","Vehicles West Spawn  Mean", expMeanW);
+    printf("\n\t%s %i\n","Bridge Length", bridge.length);
+    printf("\n\t%s %i\n","Vehicle Speed", speed);
 
-    printf("\n\t%s\n", puenteGrafic);
+    printf("\n\n%s\n","Bridge Status");
+    printf("\n\t%s %i\n","Vehicles remaining west", vehiclesW);
+    printf("\n\t%s %i\n","Vehicles remaining east", vehiclesE);
+    printf("\n\t%s %i\n","Vehicles waiting west", waitingW);
+    printf("\n\t%s %i\n","Vehicles waiting east", waitingE);
+    printf("\n\t%s %s\n","Bridge is going ", (bridge.direction == 1) ? "West to East" : "East to West");
+    printf("\n\t\t%s\n", bridge.state);
+    pthread_mutex_unlock(&sysOut);
 }
